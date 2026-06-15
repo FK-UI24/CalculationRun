c93347a9c0b169f793d0b8690fefbf075f8ac41c
FK-UI24 <gatya8gotya2gatcha9@gmail.com>
2026-06-16 05:23:10 +0900

    No5_20260616
    Unityちゃんの移動とカメラ移動のTPS化はok!!!!!!!!!!
    ここからアニメーション入れたらゲーム作り開始！！！！

--- Assets/Script/Common/Script_CameraManager.cs
+++ Assets/Script/Common/Script_CameraManager.cs
@@ -1,140 +1,75 @@
 ﻿using UnityEditor.Rendering;
 using UnityEngine;
 
-/*
- * カメラの角度をTPSのようなマウス操作で回転させる方法
- * あくまでこのスクリプトで制御しているのは基準点（Pivot）であり、カメラそのものは操作していない
- * 
- * このスクリプトはキャラクターの子オブジェクトに空オブジェクトで
- * 基準を作り（キャラの中心から少し上くらい）、その基準にアタッチする
- * キャラクター
- * 　↳基準点
- * 　　↳メインカメラ
- * 　　
- * このような構成にすることで基準点の回転→基準点の子オブジェクトのカメラも移動する
- * という感じで動いている
- * 
- */
-
-
 public class Script_CameraManager : MonoBehaviour
 {
-    //カメラが向く基準点の座標
-    [Header("カメラピボット")]
-    [SerializeField] private Transform cameraPivot;
+    [Header("カメラのターゲットなるオブジェクト")]
+    [SerializeField] private GameObject targetObject;
 
-    [Header("カメラ感度")]
-    [SerializeField] private float cameraSensitivity;
+    [Header("カメラの移動速度")]
+    [SerializeField] private float cameraSpeed;
 
-    [Header("回転の滑らかさ")]
-    [SerializeField] private float smoothSpeed;
+    [Header("カメラの上下方向の最小値")]
+    [SerializeField] private float cameraMin;
 
+    [Header("カメラの上下方向の最大値")]
+    [SerializeField] private float cameraMax;
 
+    //ターゲットのポジションを格納する用変数
+    private Vector3 targetPos;
 
-    //上下の回転角度を保存（累積）する用の変数
-    private float verticalRotation = 0f;
-
-    //左右の回転角度を保存（累積）する用の変数
-    private float horizontalRotation = 0f;
-
-
-    //基準点の初期位置・角度保存用
-    private Vector3 initialPosition;
-    private Quaternion initialRotation;
-
-
+    //上下方向の制限をするために、上下回転の合計角度を記録する用変数
+    private float verticalAngle = 0f;
 
     private void Start()
     {
+        //ターゲットの座標を格納する
+        targetPos = targetObject.transform.position;
+
         //マウスカーソルを非表示にして画面中央で固定する
         Cursor.lockState = CursorLockMode.Locked;
 
-
-        //初期位置・回転を保存
-        initialPosition = cameraPivot.localPosition;
-        initialRotation = cameraPivot.localRotation;
-
     }
 
-
-    //リセットとトリガー管理で切り替えれば、最初は視線固定、始まったら自由ができるかも
     private void Update()
     {
-        cameraControllMouse();
+        //ターゲットの移動量分、カメラも追従する
+        ///ターゲットの「前フレームからの移動量」を求め、その分だけカメラも移動させる。こうすることでカメラとターゲットの距離を一定に保つ
+        ///引かないと毎フレーム元の座標が足され続け、爆速でどこかにフライアウェイする
+        Camera.main.transform.position += targetObject.transform.position - targetPos;
 
-    }
+        //ターゲットの座標を更新する
+        targetPos = targetObject.transform.position;
 
-    //マウスでカメラ操作をする関数
-    public void cameraControllMouse()
-    {
-        //マウスの左右移動量
-        ///GetAxisは左：-1～0～1：右の範囲で返り値を返す
-        ///CameraSensitivityをかけるのは「GetAxis」「Time.deltaTime」では値が小さすぎるので調整するため
-        ///Time.deltaTimeをかけることでPC性能でそう度が変わらないようにする
-        float mouseX = Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+        //マウスの移動量を格納する
+        float mouseInputX = Input.GetAxis("Mouse X");
+        float mouseInputY = Input.GetAxis("Mouse Y");
 
-        //マウスの上下移動量
-        ///GetAxisは下：-1～0～1：上の範囲で返り値を返す
-        float mouseY = Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
+        //ターゲットの位置のY軸を中心に回転（公転）する
+        ///RotateAround(中心、軸、角度)で指定した点を中心に円を描くように回転する
+        ///Vector3.upは(0,1,0)であるため、縦方向を軸に回る=横にぐるっとまわる
+        Camera.main.transform.RotateAround(targetPos, Vector3.up, mouseInputX * Time.deltaTime * cameraSpeed);
 
-        //上下回転（カメラの上下向き）
-        //マウスを上に動かしたら上を見るためにマイナスにしている
-        //UnityではX回転（縦）が増えると下を向き、減ると上を向く。そのため直感的に操作できるようにマイナスにしている
-        //気になるならシーンビューから回転モードを選んで回してみるとわかる
-        verticalRotation -= mouseY;
+        //今のフレームで回そうとしている角度（上下）
+        float nowFrameY = mouseInputY * Time.deltaTime * cameraSpeed;
 
-        //上下の向きを制限する
-        //今回の場合マイナス方向に大きくなればどんどん上を向き、プラス方向に大きくなるとどんどん下を向く
-        //そのためここではマイナス方向の制限→上方向の制限、プラス方向への制限→下方向への制限となる
-        verticalRotation = Mathf.Clamp(verticalRotation, -40f, 20f);
+        // 前の角度保存
+        float prevAngle = verticalAngle;
 
-        //左右回転（カメラの左右向き）
-        //上下と違い左右は特に変える必要なし
-        horizontalRotation += mouseX;
+        // 角度更新
+        verticalAngle += nowFrameY;
 
-        //目標回転を作る
-        //ここではカメラが最終的に向く角度を生成している
-        //X軸回転はverticalRotation、Y軸回転はhorizontalRotation、Z軸は回転しない←これを回転目標にしている
-        ///Quaternionは回転の情報（向き）を表すデータ型→(x,y,z,w)のように４つの値で回転を表している
-        ///EulerはX軸Y軸Z軸のオイラー角を指定して、その回転におけるQuaternionを作成する
-        ///→Quaternionで定義されているメソッド
-        Quaternion targetRotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0f);
+        // Clampで制限
+        verticalAngle = Mathf.Clamp(verticalAngle, cameraMin, cameraMax);
 
-        //スムーズに補完する
-        ///localRotationは親オブジェクトから見た回転、rotationはワールド空間での回転が基準となる
-        ///今回の場合の親オブジェクトはキャラクターとなる
-        ///Slerpは回転aから回転bへ「滑らかに途中の回転」を返す
-        ///第一引数は「現在の回転」、第二引数は「目標の回転（行きたい方向）」
-        ///第三引数は「目標にどれくらいの割合近づくか」
-        cameraPivot.localRotation =
-            Quaternion.Slerp(cameraPivot.localRotation, targetRotation, smoothSpeed * Time.deltaTime);
-    }
+        // 実際に回す量（差分）
+        float delta = verticalAngle - prevAngle;
 
+        //ターゲットの上下で回転させる
+        ///Camera.main.transform.rightはカメラの右ベクトルであり、これが軸となる
+        ///→その場で回転しないか？→Roateは自分中心で回るのでその場回転するが、RotateAroundは指定した場所を中心に回るのでその場回転しない
+        Camera.main.transform.RotateAround(targetPos, Camera.main.transform.right, delta);
 
-    //基準点の位置を初期状態に戻す関数（カメラの位置を初期位置に戻す関数）
-    public void ResetPivot()
-    {
-        //初期位置に戻す
-        cameraPivot.localPosition = initialPosition;
-        cameraPivot.localRotation = initialRotation;
-
-        //変数を直す
-        //回転を角度に直す
-        Vector3 angles = initialRotation.eulerAngles;
-
-        //左右角度を反映し、横回転を戻す
-        horizontalRotation = angles.y;
-
-        //上下回転を反映し。縦回転を戻す
-        verticalRotation = angles.x;
-
-        //もし上下回転の角度が180を越えていたら
-        if (verticalRotation > 180f)
-        {
-            //その状態から-360して-180～180の間に直す
-            //こうしないとClampや回転処理で不具合が出るかも
-            verticalRotation -= 360f;
-        }
     }
 
 }
--- Assets/Script/Common/Script_PlayerCameraMove.cs
+++ Assets/Script/Common/Script_PlayerCameraMove.cs
@@ -1,6 +1,76 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class Script_PlayerCameraMove : MonoBehaviour
 {
+    [Header("プレイヤーの移動速度")]
+    [SerializeField]private float moveSpeed;
 
+    [Header("プレイヤーの回転速度")]
+    [SerializeField] private float rotateSpeed;
+
+    [Header("プレイヤー")]
+    [SerializeField] private GameObject player;
+
+    //プレイヤーのRigidBodyを扱う用変数
+    private Rigidbody playerRb;
+
+    //キーボードの入力を格納する用変数
+    //横方向
+    private float inputHorizontal;
+    //縦方向
+    private float inputVertical;
+
+    private void Start()
+    {
+        //プレイヤーのRigidBodyを取得する
+        playerRb=player.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //WASDの入力を取得している
+        ///GetAxisは滑らかに変化させる（徐々に変化させる）
+        ///GetAxisRawは即座に-1/0/1を返す（カクっと動く）
+        //A:-1～1:Dの範囲で徐々に変化させる
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        //W:1～-1:Sの範囲で徐々に変化させる
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    ///Updateは毎フレームの実行（例：60FPSなら1秒間に約60回実行する）
+    ///→入力処理やUI更新、軽い処理など人の操作に関わる処理を行う
+    ///FixedUpdateは一定時間ごとに実行される（0.02秒ごとなど、1秒間に約50回がデフォルト）
+    ///→物理演算(RigidBody)や力を加える処理、移動(物理ベース)などの安定した計算が必要な処理を行う
+    private void FixedUpdate()
+    {
+        //カメラの方向から、XZ平面の単位ベクトルを取得する
+        ///Vector3.Scaleはベクトルの各成分を掛け算する。今回はXとZはそのままでYだけ0にする→水平面だけの前方向にする
+        ///Camera.maini.transform.forwardはカメラが向いている前方向(3次元ベクトル、Y成分も含む)→例：上を向いている(0,0.7,0.7)
+        ///normalizedはベクトルの長さを１にする(正規化)→例：方向はそのままで、大きさを一定にする(2,0,2)→(0.7,0,0.7)
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        //方向キーの入力値とカメラの向きから、移動方向を決定
+        //カメラの向きに応じたベクトルを足している→Camera.Forward(0.7,0,0.7)+Camera.Right(0.7,0,-0.7)=(1.4,0,0)となったのを式化している
+        Vector3 moveForward = (cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal).normalized;
+
+        //移動方向にスピードをかける。ジャンプや落下がる場合は、別途Y軸方向のベクトルを足す
+        //Y軸は固定するとジャンプ、落下ができなくなるので上下の動きはそのまま維持している
+        playerRb.linearVelocity = moveForward * moveSpeed + new Vector3(0, playerRb.linearVelocity.y, 0);
+
+        //キャラクターの向きを進行方向にする
+        //もし移動方向がゼロじゃなかったら
+        if (moveForward != Vector3.zero)
+        {
+            ///Quaternion.LookRotationは指定した方向を向く回転を作る→その方向を見るようにする
+            Quaternion targetRotation = Quaternion.LookRotation(moveForward);
+
+            //今の向き→目標の向きに滑らかに回す
+            ///Quaternion.Slerp(現在の向き、目標の向き、どれくらい進むか(0～1))は現在の向きから目標の向きに雨らかに移動する関数
+            ///どれくらい進むか(0～1)→0はそのまま動いていない、0.5は目標まであと半分くらい、1は目標に到達
+            ///→例：rotateSpeed=10,deltaTime=0.16の場合、t=0.16となり毎フレーム「0.16%」ずつ近づく
+            player.transform.rotation = Quaternion.Slerp(player.transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+
+        }
+    }
 }
